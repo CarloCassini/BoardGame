893efa93c046bec91dcffdb31a75c2c398b0562b
CarloCassini <c.cassini87@gmail.com>
2024-01-19 13:27:33 +0100

    initialize

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
+
+// collego il foglio di scss
+import "./assets/scss/style.scss";
+
+createApp(App).mount("#app");
